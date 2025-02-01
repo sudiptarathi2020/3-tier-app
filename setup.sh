c190724eebf6ba63b12bfb6e24235b7f1cd8cc1d
sudiptarathi2020 <rathisudipta99@gmail.com>
2025-02-01 17:07:03 +0600

    Added gitignore and setup.sh file

---
+++ setup.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+
+# Exit on error
+set -e
+
+
+
+# Copy db-setup.sh to home directory
+cp $HOME/3-tier-app/db-setup.sh $HOME/
+chd $HOME
+# Make db-setup.sh executable and run it
+sudo chmod 700 db-setup.sh
+sudo ./db-setup.sh
+
+# Verify Node.js and npm
+node -v
+nvm current
+npm -v
+
+# Setup Backend
+cd $HOME/3-tier-app/backend/
+npm ci
+nohup node server.js &
+# Setup Frontend
+cd $HOME/3-tier-app/frontend/
+npm ci
+nohup run dev -- --host &
+
+# Print completion message
+echo "3-tier application successfully deployed!"
